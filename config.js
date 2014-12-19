688279a66f127a040af6e2b2854ec016e70d20d2
Josh McArthur <joshua.mcarthur@gmail.com>
2014-12-19 13:15:57 +1300

    Configure GTFS agencies for Wellington, New Zealand

--- config.js
+++ config.js
@@ -8,12 +8,17 @@ module.exports = {
   , agencies: [
     /* Put agency_key names from gtfs-data-exchange.com.
      Optionally, specify a download URL to use a dataset not from gtfs-data-exchange.com */
-    'alamedaoakland-ferry'
-    , { agency_key: 'caltrain', url: 'http://www.gtfs-data-exchange.com/agency/caltrain/latest.zip'}
-    , 'ac-transit'
-    , 'county-connection'
-    , 'san-francisco-municipal-transportation-agency'
-    , 'bay-area-rapid-transit'
-    , 'golden-gate-ferry'
+    'go-wellington',
+    'east-by-west-ferry',
+    'kapiti-coach-tours-limited',
+    'madge-coachlines-limited',
+    'mana-coach-services-ltd',
+    'newlands-coach-services',
+    'runcimans-motors-ltd',
+    'tranz-metro',
+    'tranz-scenic',
+    'tranzit-coachlines-wairarapa',
+    'valley-flyer',
+    'wellington-cable-car-ltd'
   ]
 };
